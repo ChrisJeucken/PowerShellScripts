6700c7682c70314a1288916b31df8d6b14cc7238
Chris Jeucken <christianjeucken@live.nl>
2024-11-14 14:33:31 +0100

    New synopsis and created parameter

--- 22-Citrix-Get-hosting-unit-usage/22-Citrix-Get-hosting-unit-usage.ps1
+++ 22-Citrix-Get-hosting-unit-usage/22-Citrix-Get-hosting-unit-usage.ps1
@@ -1,15 +1,28 @@
-# SCRIPT INFO -------------------
-# --- Show Citrix VAD hosting unit usage ---
-# By Chris Jeucken
-# v0.1
-# -------------------------------
-# Run on machine with Citrix Studio or Citrix VAD PowerShell SDK installed
+<#
+.SYNOPSIS
+    Show Citrix VAD hosting unit usage.
+.DESCRIPTION
+    List all Citrix VAD hosting units and show which provisioning schemes/machine catalogs use this hosting unit.
+.EXAMPLE
+    <ScriptName>.ps1 -DeliveryControllers "CTXDC1.domain.local,CTXDC2.domain.local"
+.LINK
+    https://github.com/ChrisJeucken/PowerShellScripts/tree/main/22-Citrix-Get-hosting-unit-usage
+.NOTES
+  Version:          1.0
+  Author:           Chris Jeucken
+  Creation Date:    14 November 2024
+  Website:          https://chrisjeucken.com
+.PARAMETER DeliveryControllers
+    Specify Fully Qualified Domain Name, hostname or IP address of the Citrix VAD Delivery Controllers.
+    Divide multiple DC's with a comma or don't specify this parameter if this script is run on a Delivery Controller.
+    E.g.: CTXDC1.domain.local,CTXDC2.domain.local
+#>
 
-# VARIABLES ---------------------
-# Define Citrix VAD Delivery Controllers
-    $CTXDCs = "CTXDC1.domain.local", # <-- INSERT IP ADDRESS, HOSTNAME OR FQDN FOR PRIMARY CITRIX VAD DELIVERY CONTROLLER
-            "CTXDC2.domain.local" # <-- INSERT IP ADDRESS, HOSTNAME OR FQDN FOR SECONDARY CITRIX VAD DELIVERY CONTROLLER
-# -------------------------------
+# PARAMETERS --------------------
+    param(
+        [Parameter()]
+        [string]$DeliveryControllers
+    )
 
 # PREREQUISITES -----------------
 # Import Citrix PowerShell Snapins
@@ -18,32 +31,36 @@
 
 # SCRIPT ------------------------
 # Determine working Citrix VAD Delivery Controller
-    foreach ($CTXDCTest in $CTXDCs) {
-        if (Test-Connection -ComputerName $CTXDCTest -Count 1 -ErrorAction SilentlyContinue) {
-            $CTXDC = $CTXDCTest
-            Break
+    if ($DeliveryControllers) {
+        $DeliveryControllers = $DeliveryControllers.Split(",")
+        foreach ($DeliveryController in $DeliveryControllers) {
+            if (Test-Connection -ComputerName $DeliveryController -Count 1 -ErrorAction SilentlyContinue) {
+                $TargetDC = $DeliveryController
+                Break
+            }
         }
-    }
-
-    if (!($CTXDC)) {
-        Write-Host "No working Citrix VAD Delivery Controller found. Stopping script."
-        Return
-    }
-
-# Get hypervisor connections
-    if (!(Get-Command -Name Get-BrokerHypervisorConnection -Erroraction SilentlyContinue)) {
-        Write-Host "Citrix cmdlets not found. Is Citrix Studio installed on this machine?"
-        Return
     } else {
-        $HypervisorConnections = Get-BrokerHypervisorConnection -AdminAddress $CTXDC
+        $TargetDC = "localhost"
+    } 
+
+
+    if (!($TargetDC)) {
+        Write-Error "No working Citrix VAD Delivery Controller found. Stopping script."
+        Return
+    }
+
+# Verify availability of Citrix cmdlets
+    if (!(Get-Command -Name Get-BrokerHypervisorConnection -Erroraction SilentlyContinue)) {
+        Write-Error "Citrix cmdlets not found. Is Citrix Studio installed on this machine?"
+        Return
     }
 
 # Get hosting units
     if (!(Get-PSDrive -Name "XDHyp")) {
-        Write-Host "XDHyp PSDrive does not exist. Have the Citrix PowerShell snapins loaded correctly?"
+        Write-Error "XDHyp PSDrive does not exist. Have the Citrix PowerShell snapins loaded correctly?"
         Return
     } else {
-        $HostingUnits = Get-ChildItem "XDHyp:\HostingUnits\" -AdminAddress $CTXDC
+        $HostingUnits = Get-ChildItem "XDHyp:\HostingUnits\" -AdminAddress $TargetDC
     }
 
 # Get provisioning schemes
